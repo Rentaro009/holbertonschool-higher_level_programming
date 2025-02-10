8a4e76bd6e2b6568d982e119a48b8b5cf9dd9aee
Rentaro009 <r3ntaro0@gmail.com>
2025-02-10 10:25:52 -0400

    fixed a another mistake in the code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     argc = len(argv)
 
     if argc == 0:
-        print("0 argumnets. ")
+        print("0 argumnets.")
     elif argc == 1:
         print("1 arguments:")
     else:
